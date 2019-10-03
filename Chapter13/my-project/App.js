9c207181fc4d559064c6a71e669d6d65840b5fde
Adam Boduch <adam.boduch@gmail.com>
2019-10-03 11:56:04 -0400

    Chapter 13

---
+++ Chapter13/my-project/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>
+        Open up App.js to start working on your app!
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  text: {
+    fontWeight: "bold"
+  }
+});
